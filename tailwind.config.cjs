83b585593b3b2cfb381efc1042ba7003cacd61b3
Elvis Saavedra <elvis.saavedra@gridunity.com>
2023-08-05 13:24:41 -0500

    split in different components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
     fontFamily: {
-      sans: ['Gotham Medium', 'sans-serif']
+      sans: ['Gotham Medium', 'sans-serif'],
     },
-		extend: {},
-	},
-	plugins: [],
+    extend: {},
+  },
+  plugins: [],
 }
